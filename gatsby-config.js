72ba4fd15fd932c8e887f77d39755d7c72df76eb
Theo Vickers <theo.vickers777@gmail.com>
2023-06-21 23:12:24 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Theo Website June 2023`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
